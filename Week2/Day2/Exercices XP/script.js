6a0e35ead993ab07a42a82aa978fa0a201cf7d6e
IbrahimaBANDE <ibrahimabande1104@gmail.com.com>
2022-08-29 18:43:50 +0100

    Bootcamp De Codage Full Stack - Python - Temps Plein

---
+++ Week2/Day2/Exercices XP/script.js
@@ -0,0 +1,65 @@
+//exercice1
+    /*Code
+    let x=3;
+    let y=1;
+    if (x>y)
+        {
+        console.log("x is the biggest number")
+        }
+    else
+        {
+        console.log("y is the biggest number")
+        }  
+    */
+
+//exercice2
+    /*Code
+        let newDog ="Chihuahua";
+        newDog_length=newDog.length;
+        console.log(newDog_length);
+        console.log(newDog.toUpperCase());
+        console.log(newDog.toLowerCase());
+        if (newDog=="Chihuahua")
+                {
+                    console.log("J'adore les chihuahuas, c'est ma race de chien préférée");
+                }
+        else
+                {
+                    console.log("Je m'en fous, je préfère les chats");
+                }
+                
+            */
+
+//exercice3
+    /*Code
+        let val=prompt("Saisir une valeur:");
+        if(val%2==0){
+            console.log(val,"est un nombre pair. Où ",val," est le nombre réel choisi par l'utilisateur.");
+        }   
+        else
+        {
+            console.log(val,"est un nombre impair. Où ",val," est le nombre réel choisi par l'utilisateur.");
+        }
+    */
+
+//exercice3
+/Code
+let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+if(users.length==0)
+{
+    console.log("no one is online");
+}
+else if(users.length==1)
+{
+    console.log(users[0],"is online");
+}
+else if(users.length==2)
+{
+    console.log(users[0], "and" ,users[1],"are online");
+}
+else
+{
+    console.log(users[0], "," ,users[1], "and",users.length-2, "are online");
+}
+*/
+
